67ccdc4ab4516d04dd312274790441014f1eb57b
sckara <sbmitrovic@gmail.com>
2025-05-05 01:17:14 +0200

    Search added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,53 @@
+// App.js
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ImagesList from './components/imagesList';
+import SearchButton from './components/searchButton';
 import axios from 'axios';
-import { useState } from 'react';
 
 const PAGE_NUMBER = 1;
+
 function App() {
-  
   const [imagesData, setImagesData] = useState([]);
+  const [originalImagesData, setOriginalImagesData] = useState([]);
   const [page, setPage] = useState(PAGE_NUMBER);
 
   useEffect(() => {
-    setTimeout(async () => {
+    const fetchImages = async () => {
       const response = await axios.get(
         `https://picsum.photos/v2/list?page=${page}`
       );
 
-      setImagesData((oldState) => {
-        return [...oldState, ...response.data];
-      });
-    }, 1500);
-  }, [page]);
+      setImagesData((old) => [...old, ...response.data]);
+      setOriginalImagesData((old) => [...old, ...response.data]);
+    };
 
+    setTimeout(fetchImages, 1500);
+  }, [page]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleScroll = async () => {
+  const handleScroll = () => {
     if (
-        window.innerHeight + document.documentElement.scrollTop + 1 >=
-        document.documentElement.scrollHeight
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
     ) {
-        setPage((prev) => prev + 1);
+      setPage((prev) => prev + 1);
     }
-};
+  };
 
   return (
     <div className='app'>
+      <SearchButton
+        originalImages={originalImagesData}
+        setFilteredImages={setImagesData}
+      />
       <h1>Images Gallery</h1>
       <ImagesList imagesData={imagesData} />
     </div>
-
-
   );
 }
 
---
+++ my-app/src/components/searchButton.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import "./searchButton.css"
+function SearchButton({ originalImages, setFilteredImages }) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleClick = () => {
+    const filtered = originalImages.filter((img) => {
+        const nameParts = img.author.toLowerCase().split(' ');
+        return nameParts.some((part) => part === inputValue.toLowerCase());
+      });
+
+    window.alert(`Found ${filtered.length} result(s) for: ${inputValue}`);
+    setFilteredImages(filtered);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by author"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleClick}>Search</button>
+    </div>
+  );
+}
+
+export default SearchButton;
